a963d9e569a160bd958b056c638aab4b248eda13
mostafa Dahli <mostafadahli88@gmail.com>
2022-11-15 09:03:37 +0200

    INIT

---
+++ app/src/main/java/com/example/accountsign/Adjectives.java
@@ -0,0 +1,41 @@
+package com.example.accountsign;
+
+public class Adjectives {
+    private int Age;
+
+    public int getAge() {
+        return Age;
+    }
+
+    public void setAge(int age) {
+        Age = age;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getGender() {
+        return Gender;
+    }
+
+    public void setGender(String gender) {
+        Gender = gender;
+    }
+
+    public int getMoneycounter() {
+        return Moneycounter;
+    }
+
+    public void setMoneycounter(int moneycounter) {
+        Moneycounter = moneycounter;
+    }
+
+    private String Name;
+    private String Gender;
+    private int Moneycounter;
+}
